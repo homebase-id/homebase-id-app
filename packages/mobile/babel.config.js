73fa6bbe9778e74ebd782e82312c84442e9905da
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-03-01 16:16:45 +0530

    add react native animated

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  // plugins: [
-  //   [
-  //     'module-resolver',
-  //     {
-  //       alias: {
-  //         stream: 'stream-browserify',
-  //         buffer: '@craftzdog/react-native-buffer',
-  //         crypto: 'react-native-quick-crypto',
-  //       },
-  //     },
-  //   ],
-  // ],
+  plugins: ['react-native-reanimated/plugin'],
 };
