a8b82dbe3ac526f45fa064095e0ff87a4bac922d
Sebastian Bargmann <sebbarg@gmail.com>
2024-03-18 15:14:25 +0100

    fetch device token on permission granted

--- packages/mobile/src/components/Notification/NotificationProvider.tsx
+++ packages/mobile/src/components/Notification/NotificationProvider.tsx
@@ -1,7 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import notifee from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const NotificationProvider = () => {
+  const [permissionGranted, setPermissionGranted] = useState(false);
+
+  // iOS: OK
+  // Android: OK
+  //
+  // Request permission to use notifications
   useEffect(() => {
     const requestUserPermission = async () => {
       try {
@@ -9,8 +16,10 @@ const NotificationProvider = () => {
 
         if (status) {
           console.log('Notification permission granted.');
+          setPermissionGranted(true);
         } else {
           console.log('Notification permission denied.');
+          setPermissionGranted(false);
         }
       } catch (error) {
         console.error(error);
@@ -19,6 +28,27 @@ const NotificationProvider = () => {
 
     requestUserPermission();
   }, []);
+
+  //
+
+  // iOS: OK (assuming notification permission granted)
+  // Android: OK
+  //
+  // Get the device token
+  useEffect(() => {
+    if (permissionGranted) {
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log('FCM Token:', token);
+        })
+        .catch((error) => {
+          console.error('Error fetching FCM Token:', error);
+        });
+    }
+  }, [permissionGranted]);
+
+  //
 
   // This component doesn't render anything
   return null;
