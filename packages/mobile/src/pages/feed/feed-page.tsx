bdfd6d847c9960553796d1117034a1afd90fd218
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-03-20 10:51:54 +0530

    Don't zoom in feed app when in webview

--- packages/mobile/src/pages/feed/feed-page.tsx
+++ packages/mobile/src/pages/feed/feed-page.tsx
@@ -26,6 +26,7 @@ export const FeedPage = memo((_props: FeedProps) => {
 
   const INJECTED_JAVASCRIPT = useMemo(() => {
     return `(function() {
+       const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);
         const APP_SHARED_SECRET_KEY = 'APPS_feed';
         const APP_AUTH_TOKEN_KEY = 'BX0900_feed';
         const IDENTITY_KEY = 'identity';
