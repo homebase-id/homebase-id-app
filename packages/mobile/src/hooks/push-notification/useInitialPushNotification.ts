f2106383cf4317ec2a96346488d65abf0fc77a3f
Stef Coenen <stef@thebrownfox.be>
2024-04-17 06:07:57 +0200

    Added mail app icon with unread dot; (#38)
    * Added mail app icon with unread dot;
    
    * Added mail app icon with unread dot;

--- packages/mobile/src/hooks/push-notification/useInitialPushNotification.ts
+++ packages/mobile/src/hooks/push-notification/useInitialPushNotification.ts
@@ -1,19 +1,21 @@
 import messaging from '@react-native-firebase/messaging';
 import { NavigationProp, useFocusEffect, useNavigation } from '@react-navigation/native';
 import { tryJsonParse } from '@youfoundation/js-lib/helpers';
-import { navigateOnNotification } from '../../components/Notifications/NotificationsOverview';
-import { AppStackParamList, TabStackParamList } from '../../app/App';
+import { navigateOnNotification } from '../../components/Dashboard/NotificationsOverview';
+import { ChatStackParamList, TabStackParamList } from '../../app/App';
 import { PushNotification } from '@youfoundation/js-lib/core';
 import { useDotYouClientContext } from 'feed-app-common';
 import { useEffect } from 'react';
 
 export const useInitialPushNotification = () => {
   const identity = useDotYouClientContext().getIdentity();
-  const chatNavigator = useNavigation<NavigationProp<AppStackParamList>>();
+  const chatNavigator = useNavigation<NavigationProp<ChatStackParamList>>();
   const feedNavigator = useNavigation<NavigationProp<TabStackParamList>>();
   const getInitialNotification = () => {
     (async () => {
-      const initialNotification = await messaging().getInitialNotification() || await new Promise(resolve => messaging().onNotificationOpenedApp(resolve));
+      const initialNotification =
+        (await messaging().getInitialNotification()) ||
+        (await new Promise((resolve) => messaging().onNotificationOpenedApp(resolve)));
       if (
         initialNotification &&
         initialNotification.data?.data &&
