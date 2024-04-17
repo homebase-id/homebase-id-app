5e7d89421ee67748e490b1eb63903ae797a9c56f
Stef Coenen <stef@thebrownfox.be>
2024-04-17 15:45:01 +0200

    Extend FileOverview with video "preview"

--- packages/mobile/src/components/Files/FileOverview.tsx
+++ packages/mobile/src/components/Files/FileOverview.tsx
@@ -1,7 +1,8 @@
 import { ScrollView, View, ImageBackground, TouchableOpacity } from 'react-native';
 import { Asset } from 'react-native-image-picker';
-import { Close } from '../ui/Icons/icons';
+import { Close, Play } from '../ui/Icons/icons';
 import React, { memo } from 'react';
+import { Colors } from '../../app/Colors';
 
 export const FileOverview = memo(
   ({ assets, setAssets }: { assets: Asset[]; setAssets: (newAssets: Asset[]) => void }) => {
@@ -14,7 +15,7 @@ export const FileOverview = memo(
         showsHorizontalScrollIndicator={false}
       >
         {assets.map((value, index) => {
-          // const isVideo = value.type?.startsWith('video') ?? false;
+          const isVideo = value.type?.startsWith('video') ?? false;
           return (
             <View
               key={index}
@@ -22,20 +23,40 @@ export const FileOverview = memo(
                 borderRadius: 15,
               }}
             >
-              <ImageBackground
-                key={index}
-                source={{ uri: value.uri || value.originalPath }}
-                style={{
-                  width: 65,
-                  height: 65,
-                  alignItems: 'flex-end',
-                  padding: 4,
-                }}
-              >
-                <TouchableOpacity onPress={() => setAssets(assets.filter((_, i) => i !== index))}>
-                  <Close size={'sm'} color="white" />
-                </TouchableOpacity>
-              </ImageBackground>
+              {isVideo ? (
+                <View
+                  style={{
+                    backgroundColor: Colors.slate[200],
+                    width: 65,
+                    height: 65,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Play size={'md'} color={Colors.slate[800]} />
+                  <TouchableOpacity
+                    onPress={() => setAssets(assets.filter((_, i) => i !== index))}
+                    style={{ position: 'absolute', top: 4, right: 4 }}
+                  >
+                    <Close size={'sm'} color="white" />
+                  </TouchableOpacity>
+                </View>
+              ) : (
+                <ImageBackground
+                  key={index}
+                  source={{ uri: value.uri || value.originalPath }}
+                  style={{
+                    width: 65,
+                    height: 65,
+                    alignItems: 'flex-end',
+                    padding: 4,
+                  }}
+                >
+                  <TouchableOpacity onPress={() => setAssets(assets.filter((_, i) => i !== index))}>
+                    <Close size={'sm'} color="white" />
+                  </TouchableOpacity>
+                </ImageBackground>
+              )}
             </View>
           );
         })}
