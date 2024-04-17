3053112ecd4836f7c1a56cdcbd854f77677ffbc1
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-04-17 17:35:41 +0530

    Refresh time every one minute on conversation tile

--- packages/mobile/src/components/Chat/Chat-Sent-Time-Indicator.tsx
+++ packages/mobile/src/components/Chat/Chat-Sent-Time-Indicator.tsx
@@ -1,6 +1,6 @@
 import { HomebaseFile } from '@youfoundation/js-lib/core';
 import { ChatMessage } from '../../provider/chat/ChatProvider';
-import { memo } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 import { Text } from 'react-native';
 import { formatToTimeAgoWithRelativeDetail } from 'feed-app-common';
 import { Colors } from '../../app/Colors';
@@ -9,13 +9,25 @@ import { useDarkMode } from '../../hooks/useDarkMode';
 export const ChatSentTimeIndicator = memo(
   ({ msg, keepDetail }: { msg: HomebaseFile<ChatMessage>; keepDetail?: boolean }) => {
     const { isDarkMode } = useDarkMode();
-    if (!msg.fileMetadata.created) return null;
+    const date = useMemo(() => new Date(msg.fileMetadata.created), [msg.fileMetadata.created]);
+    const [formattedDate, setformattedDate] = useState<string | undefined>(
+      formatToTimeAgoWithRelativeDetail(date, keepDetail)
+    );
 
-    const date = new Date(msg.fileMetadata.created);
-    if (!date) return <Text>Unknown</Text>;
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const newFormattedDate = formatToTimeAgoWithRelativeDetail(date, keepDetail);
+        if (newFormattedDate !== formattedDate) {
+          setformattedDate(newFormattedDate);
+        }
+      }, 1000 * 60);
+      return () => clearInterval(interval);
+    }, [date, formattedDate, keepDetail]);
+
+    if (!msg.fileMetadata.created) return null;
     return (
       <Text style={{ fontSize: 12, color: isDarkMode ? Colors.slate[300] : Colors.slate[500] }}>
-        {formatToTimeAgoWithRelativeDetail(date, keepDetail)}
+        {formattedDate || 'Unknown'}
       </Text>
     );
   }
