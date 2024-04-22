b586646aecaa18a80341af4dd9be29aa7f58f95e
Stef Coenen <stef@thebrownfox.be>
2024-04-22 14:00:05 +0200

    Added extra info when unknown appId in notifications; (#47)

--- packages/mobile/src/app/constants.ts
+++ packages/mobile/src/app/constants.ts
@@ -3,3 +3,4 @@ export const MAIL_APP_ID = '6e8ecfff-7c15-40e4-94f4-d6e83bfb5857';
 export const FEED_APP_ID = '5f887d80-0132-4294-ba40-bda79155551d';
 export const PHOTO_APP_ID = '32f0bdbf-017f-4fc0-8004-2d4631182d1e';
 export const OWNER_APP_ID = 'ac126e09-54cb-4878-a690-856be692da16';
+export const FEED_CHAT_APP_ID = 'b4a2a939-45d3-42af-95bf-7d241016e3bf';
