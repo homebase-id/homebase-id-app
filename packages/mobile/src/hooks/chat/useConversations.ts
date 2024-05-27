556101b4044617c5c3d3af218968a706e28be08c
Stef Coenen <stef@thebrownfox.be>
2024-05-27 16:03:33 +0200

    The branch name really says it all; (#91)
    * The branch name really says it all;
    
    * Revert busted cache;

--- packages/mobile/src/hooks/chat/useConversations.ts
+++ packages/mobile/src/hooks/chat/useConversations.ts
@@ -1,10 +1,7 @@
 import { UnifiedConversation, getConversations } from '../../provider/chat/ConversationProvider';
-import { InfiniteData, QueryClient, useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
+import { InfiniteData, QueryClient, useInfiniteQuery } from '@tanstack/react-query';
 import { HomebaseFile } from '@youfoundation/js-lib/core';
 import { useDotYouClientContext } from 'feed-app-common';
-import { ChatMessage } from '../../provider/chat/ChatProvider';
-import { useCallback, useEffect, useState } from 'react';
-import { getChatMessageInfiniteQueryOptions } from './useChatMessages';
 import { stringGuidsEqual } from '@youfoundation/js-lib/helpers';
 
 const PAGE_SIZE = 500;
@@ -26,84 +23,6 @@ export const useConversations = () => {
       refetchOnMount: false,
       staleTime: 1000 * 60 * 5, // 5min before conversations from another device are fetched on this one
     }),
-  };
-};
-
-export type ConversationWithRecentMessage = HomebaseFile<UnifiedConversation> & {
-  lastMessage: HomebaseFile<ChatMessage> | null;
-};
-
-export const useConversationsWithRecentMessage = () => {
-  const dotYouClient = useDotYouClientContext();
-  const queryClient = useQueryClient();
-
-  const { data: conversations, ...rest } = useConversations().all;
-
-  const [conversationsWithRecent, setConversationsWithRecent] = useState<
-    ConversationWithRecentMessage[]
-  >([]);
-
-  const buildConversationsWithRecent = useCallback(
-    async (
-      conversations:
-        | InfiniteData<
-            {
-              searchResults: HomebaseFile<UnifiedConversation>[];
-              cursorState: string;
-              queryTime: number;
-              includeMetadataHeader: boolean;
-            } | null,
-            unknown
-          >
-        | undefined
-    ) => {
-      const flatConversations =
-        (conversations?.pages
-          ?.flatMap((page) => page?.searchResults)
-          .filter(
-            (convo) => convo && [0, undefined].includes(convo.fileMetadata.appData.archivalStatus)
-          ) as ConversationWithRecentMessage[]) || [];
-
-      if (!flatConversations || !flatConversations || flatConversations.length === 0) {
-        return flatConversations;
-      }
-
-      const convoWithMessage: ConversationWithRecentMessage[] = await Promise.all(
-        (flatConversations.filter(Boolean) as HomebaseFile<UnifiedConversation>[]).map(
-          async (convo) => {
-            const conversationId = convo.fileMetadata.appData.uniqueId;
-            const messagesA = await queryClient.fetchInfiniteQuery(
-              getChatMessageInfiniteQueryOptions(dotYouClient, conversationId)
-            );
-            return {
-              ...convo,
-              lastMessage: messagesA.pages[0].searchResults[0],
-            };
-          }
-        )
-      );
-
-      convoWithMessage.sort((a, b) => {
-        if (!a.lastMessage) return -1;
-        if (!b.lastMessage) return 1;
-        return b.lastMessage.fileMetadata.created - a.lastMessage.fileMetadata.created;
-      });
-      return convoWithMessage;
-    },
-    [dotYouClient, queryClient]
-  );
-
-  useEffect(() => {
-    buildConversationsWithRecent(conversations).then((conversationsWithRecent) => {
-      setConversationsWithRecent(conversationsWithRecent);
-    });
-  }, [conversations, buildConversationsWithRecent]);
-
-  return {
-    all: {
-      ...rest,
-      data: conversationsWithRecent,
-    },
   };
 };
 
