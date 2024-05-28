3ea89d0077a59e1029e2f41e6c858fafb7c8a493
Stef Coenen <stef@thebrownfox.be>
2024-05-28 11:23:18 +0200

    Added imagenotification extension for iOS; (#92)

---
+++ packages/mobile/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Stef Coenen on 28/05/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
