60c965e50606bab91e187cc37f24c1ca44f1a35d
Stef Coenen <stef@thebrownfox.be>
2024-06-17 10:34:37 +0200

    Open non feed-app urls in the browser;

--- packages/mobile/src/pages/feed/feed-page.tsx
+++ packages/mobile/src/pages/feed/feed-page.tsx
@@ -5,7 +5,7 @@ import { TabStackParamList } from '../../app/App';
 import { SafeAreaView } from '../../components/ui/SafeAreaView/SafeAreaView';
 import WebView from 'react-native-webview';
 import { uint8ArrayToBase64 } from '@youfoundation/js-lib/helpers';
-import { Keyboard, RefreshControl, TouchableOpacity } from 'react-native';
+import { Keyboard, Linking, RefreshControl, TouchableOpacity } from 'react-native';
 import { useAuth } from '../../hooks/auth/useAuth';
 import { Colors } from '../../app/Colors';
 import { useDarkMode } from '../../hooks/useDarkMode';
@@ -42,26 +42,22 @@ export const FeedPage = memo((_props: FeedProps) => {
   const uri = useMemo(() => `https://${identity}/apps/feed`, [identity]);
   const originWhitelist = useMemo(() => [`https://${identity}`], [identity]);
 
-  // const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);
-  // @2002Bishwajeet: this ☝️ ain't right.. It breaks the injected_javascript fully (I assume as the JS is set to load before the content);
-  //  To be honest Not sure if we want it, and if we do, it should be part of the feed-app itself, enabling it based on the "client_type" key in the localStorage
-
   const INJECTED_JAVASCRIPT = useMemo(() => {
     return `(function() {
         const APP_SHARED_SECRET_KEY = 'APPS_feed';
         const APP_AUTH_TOKEN_KEY = 'BX0900_feed';
-        const IDENTITY_KEY = 'identity';
+        // const IDENTITY_KEY = 'identity';
         const APP_CLIENT_TYPE_KEY = 'client_type';
         const PREFERS_DARK_MODE = 'prefersDark'
 
         const APP_SHARED_SECRET = '${base64SharedSecret}';
         const APP_AUTH_TOKEN = '${authToken}';
-        const IDENTITY = '${identity}';
+        // const IDENTITY = '${identity}';
         const APP_CLIENT_TYPE = 'react-native-v2';
 
         window.localStorage.setItem(APP_SHARED_SECRET_KEY, APP_SHARED_SECRET);
         window.localStorage.setItem(APP_AUTH_TOKEN_KEY, APP_AUTH_TOKEN);
-        window.localStorage.setItem(IDENTITY_KEY, IDENTITY);
+        // window.localStorage.setItem(IDENTITY_KEY, IDENTITY);
         window.localStorage.setItem(APP_CLIENT_TYPE_KEY, APP_CLIENT_TYPE);
         window.localStorage.setItem(PREFERS_DARK_MODE, '${isDarkMode ? '1' : '0'}');
       })();`;
@@ -82,10 +78,7 @@ export const FeedPage = memo((_props: FeedProps) => {
     }
   };
 
-  const doCloseComposer = useCallback(() => {
-    setIsPostComposerOpen(false);
-  }, []);
-
+  const doCloseComposer = useCallback(() => setIsPostComposerOpen(false), []);
   const doCloseAndRefresh = useCallback(() => {
     setIsPostComposerOpen(false);
     webviewRef.current?.reload();
@@ -175,6 +168,17 @@ export const FeedPage = memo((_props: FeedProps) => {
                   add(error);
                 }}
                 renderError={renderError}
+                allowsBackForwardNavigationGestures={true}
+                onNavigationStateChange={(navState) => {
+                  const targetUrl = navState.url;
+                  const shouldMoveToNewWindow = !targetUrl.startsWith(uri);
+                  if (targetUrl && shouldMoveToNewWindow) {
+                    // Disable loading any page that is not part of the feed-app; And open it in the browser
+                    webviewRef.current?.stopLoading();
+                    webviewRef.current?.goBack();
+                    Linking.openURL(targetUrl);
+                  }
+                }}
               />
               {!keyboardVisible ? (
                 <TouchableOpacity
