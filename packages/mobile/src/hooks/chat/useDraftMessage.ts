47a9cb125f4109ca5ba1188b57ba8aa7f6e75c0b
Stef Coenen <stef@thebrownfox.be>
2024-07-02 15:45:15 +0200

    Better callbacks (#129)
    * Better callbacks
    
    * Fix for empty optimistic image;
    
    * better handling of Name preview and show first name only
    
    ---------
    
    Co-authored-by: Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>

--- packages/mobile/src/hooks/chat/useDraftMessage.ts
+++ packages/mobile/src/hooks/chat/useDraftMessage.ts
@@ -3,43 +3,39 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
 
 export const useDraftMessage = (conversationId: string | undefined) => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const getDraftMessage = useCallback(async (): Promise<string> => {
-        return await AsyncStorage.getItem(`draft-${conversationId}`) || '';
-    }
-        , [conversationId]);
+  const getDraftMessage = useCallback(async (): Promise<string> => {
+    return (await AsyncStorage.getItem(`draft-${conversationId}`)) || '';
+  }, [conversationId]);
 
-    async function setDraftMessage(message: string): Promise<void> {
-        await AsyncStorage.setItem(`draft-${conversationId}`, message);
-    }
-    return {
-        set: useMutation({
-            mutationKey: ['draft', conversationId],
-            mutationFn: setDraftMessage,
-            gcTime: 0,
-            onSettled: () => {
-                queryClient.invalidateQueries({ queryKey: ['draft', conversationId] });
-            },
-        }),
-        getDraftMessage,
-
-    };
+  async function setDraftMessage(message: string): Promise<void> {
+    await AsyncStorage.setItem(`draft-${conversationId}`, message);
+  }
+  return {
+    set: useMutation({
+      mutationKey: ['draft', conversationId],
+      mutationFn: setDraftMessage,
+      gcTime: 0,
+      onSettled: () => {
+        queryClient.invalidateQueries({ queryKey: ['draft', conversationId] });
+      },
+    }),
+    getDraftMessage,
+  };
 };
 
 export const useDraftMessageValue = (conversationId: string | undefined) => {
-    const getDraftMessage = useCallback(async (): Promise<string> => {
-        return await AsyncStorage.getItem(`draft-${conversationId}`) || '';
-    }
-        , [conversationId]);
-    return {
-        get: useQuery({
-            queryKey: ['draft', conversationId],
-            queryFn: getDraftMessage,
-            enabled: !!conversationId,
-            staleTime: 0,
-            gcTime: 0,
-        }),
-    };
+  const getDraftMessage = useCallback(async (): Promise<string> => {
+    return (await AsyncStorage.getItem(`draft-${conversationId}`)) || '';
+  }, [conversationId]);
+  return {
+    get: useQuery({
+      queryKey: ['draft', conversationId],
+      queryFn: getDraftMessage,
+      enabled: !!conversationId,
+      staleTime: 0,
+      gcTime: 0,
+    }),
+  };
 };
-
