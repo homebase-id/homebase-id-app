f2494eddc6462aa3c5aad9e09b19c52d8cfada6a
Stef Coenen <stef@thebrownfox.be>
2024-07-02 16:21:29 +0200

    Fix semi endless loop for marking notifications as read;

--- packages/mobile/src/hooks/notifications/usePushNotifications.ts
+++ packages/mobile/src/hooks/notifications/usePushNotifications.ts
@@ -50,7 +50,7 @@ export const usePushNotifications = (props?: { appId?: string }) => {
           ...existingData,
           results: existingData.results.map((n) => ({
             ...n,
-            unread: !notificationIds.some((id) => id === n.id),
+            unread: notificationIds.some((id) => id === n.id) ? false : n.unread,
           })),
         };
         queryClient.setQueryData(['push-notifications'], newData);
