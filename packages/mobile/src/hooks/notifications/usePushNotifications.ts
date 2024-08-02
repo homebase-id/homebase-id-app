e983e361c1ba8d4e921d807caf94d7b6828cda2e
Stef Coenen <stef@thebrownfox.be>
2024-08-02 17:24:13 +0200

    Optional disable;

--- packages/mobile/src/hooks/notifications/usePushNotifications.ts
+++ packages/mobile/src/hooks/notifications/usePushNotifications.ts
@@ -107,7 +107,7 @@ export const useUnreadPushNotificationsCount = (props?: { appId?: string }) => {
   });
 };
 
-export const useRemoveNotifications = (props: { disabled: boolean; appId: string }) => {
+export const useRemoveNotifications = (props: { disabled?: boolean; appId: string }) => {
   const dotYouClient = useDotYouClientContext();
   const queryClient = useQueryClient();
   const { data: unreadCount } = useUnreadPushNotificationsCount({ appId: props.appId });
