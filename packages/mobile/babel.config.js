cd73b56e712b0c8572f8be776087a80f1ad8fde5
Stef Coenen <stef@thebrownfox.be>
2024-08-23 10:21:50 +0200

    Restored remove console;

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: ['react-native-reanimated/plugin'],
-  // env: {
-  //   production: {
-  //     plugins: ['transform-remove-console'],
-  //   },
-  // },
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
