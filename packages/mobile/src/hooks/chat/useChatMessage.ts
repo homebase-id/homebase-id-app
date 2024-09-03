fdbad2c48f0d183068ac2298a03a77d4154766d1
Stef Coenen <stef@thebrownfox.be>
2024-09-03 16:06:06 +0200

    Fixed for failed messages to get removed from cache; (#181)

--- packages/mobile/src/hooks/chat/useChatMessage.ts
+++ packages/mobile/src/hooks/chat/useChatMessage.ts
@@ -138,7 +138,8 @@ const sendMessage = async ({
   newChat.fileId = uploadResult.file.fileId;
   newChat.fileMetadata.versionTag = uploadResult.newVersionTag;
   newChat.fileMetadata.appData.previewThumbnail = uploadResult.previewThumbnail;
-  newChat.fileMetadata.appData.content.deliveryStatus = ChatDeliveryStatus.Sent;
+  newChat.fileMetadata.appData.content.deliveryStatus =
+    uploadResult.chatDeliveryStatus || ChatDeliveryStatus.Sent;
 
   return newChat;
 };
