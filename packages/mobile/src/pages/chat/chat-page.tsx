c174432f6a2d8e9056b3e12813a2a4c0e047fac9
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-09-16 14:06:40 +0530

    fix imports

--- packages/mobile/src/pages/chat/chat-page.tsx
+++ packages/mobile/src/pages/chat/chat-page.tsx
@@ -13,7 +13,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import { ChatAppBar, SelectedMessageProp } from '../../components/Chat/Chat-App-Bar';
+import { ChatAppBar, SelectedMessageProp } from '../../components/Chat/Chat-app-bar';
 import { Asset } from 'react-native-image-picker';
 import {
   ChatDeletedArchivalStaus,
