98902da7552dbc3f299aa6a5a86b70b68c1e1502
Stef Coenen <stef@thebrownfox.be>
2024-09-20 15:02:24 +0200

    Unrelated copy change;

--- packages/mobile/src/components/Dashboard/NotificationsOverview.tsx
+++ packages/mobile/src/components/Dashboard/NotificationsOverview.tsx
@@ -301,7 +301,7 @@ export const bodyFormer = (
     } else if (payload.options.typeId === OWNER_CONNECTION_ACCEPTED_TYPE_ID) {
       return `${sender} accepted your connection request`;
     } else if (payload.options.typeId === OWNER_INTRODUCTION_RECEIVED_TYPE_ID) {
-      return `${sender} was introduced to you`;
+      return `${sender} introduced you to someone`;
     } else if (payload.options.typeId === OWNER_INTRODUCTION_ACCEPTED_TYPE_ID) {
       return `${sender} confirmed the introduction`;
     }
