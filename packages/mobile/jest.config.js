abbf24a1d22c4da0e9e0a235183d9740bbe7f208
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-09-26 15:57:36 +0530

    utils test initialise

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@homebase-id|@tanstack/react-query)/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
