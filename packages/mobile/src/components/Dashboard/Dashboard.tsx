417817455f73357c3d4b281965a9a7fb767a6a50
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-12-17 20:33:24 +0530

    feat: community app shortcut in dashboard

--- packages/mobile/src/components/Dashboard/Dashboard.tsx
+++ packages/mobile/src/components/Dashboard/Dashboard.tsx
@@ -1,28 +1,38 @@
 import { useDotYouClientContext } from 'homebase-id-app-common';
 import { FC, memo } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, ViewStyle } from 'react-native';
 import { openURL } from '../../utils/utils';
-import { BrandIconProps, Homebase, HomebaseMail } from '../ui/Icons/brandIcons';
+import { BrandIconProps, Homebase, HomebaseCommunity, HomebaseMail } from '../ui/Icons/brandIcons';
 import { useUnreadPushNotificationsCount } from '../../hooks/notifications/usePushNotifications';
-import { MAIL_APP_ID, OWNER_APP_ID } from '../../app/constants';
+import { COMMUNITY_APP_ID, MAIL_APP_ID, OWNER_APP_ID } from '../../app/constants';
 import { Colors } from '../../app/Colors';
 import { useDarkMode } from '../../hooks/useDarkMode';
 import { Text } from '../ui/Text/Text';
 
 export const Dashboard = memo(() => {
   return (
-    <View
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 6,
-        marginVertical: 16,
-      }}
-    >
-      <AppLink label="Owner console" appPath="owner" appId={OWNER_APP_ID} icon={Homebase} />
-      <AppLink label="Mail" appPath="apps/mail" appId={MAIL_APP_ID} icon={HomebaseMail} />
-    </View>
+    <>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          gap: 6,
+          marginVertical: 16,
+        }}
+      >
+        <AppLink label="Owner console" appPath="owner" appId={OWNER_APP_ID} icon={Homebase} />
+        <AppLink label="Mail" appPath="apps/mail" appId={MAIL_APP_ID} icon={HomebaseMail} />
+      </View>
+      <AppLink
+        label="Community"
+        appPath="apps/community"
+        appId={COMMUNITY_APP_ID}
+        icon={HomebaseCommunity}
+        style={{ width: '50%', marginBottom: 16 }}
+      />
+    </>
   );
 });
 
@@ -31,6 +41,7 @@ const AppLink = (props: {
   appPath: string;
   appId: string;
   icon: FC<BrandIconProps>;
+  style?: ViewStyle;
 }) => {
   const { isDarkMode } = useDarkMode();
   const identity = useDotYouClientContext().getLoggedInIdentity();
@@ -48,11 +59,11 @@ const AppLink = (props: {
         position: 'relative',
         padding: 8,
         backgroundColor: isDarkMode ? Colors.black : Colors.white,
-
         borderRadius: 10,
         borderStyle: 'solid',
         borderColor: isDarkMode ? Colors.slate[700] : Colors.slate[200],
         borderWidth: 1,
+        ...props.style,
       }}
       onPress={() => openURL(`https://${identity}/${props.appPath}`)}
     >
