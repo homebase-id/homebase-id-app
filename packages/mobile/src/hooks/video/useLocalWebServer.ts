b9005938906344a1ddca560eb4d892c9a309e178
Stef Coenen <stef@thebrownfox.be>
2025-07-21 14:55:56 +0200

    Restored react-native-http-bridge-refurbished;

--- packages/mobile/src/hooks/video/useLocalWebServer.ts
+++ packages/mobile/src/hooks/video/useLocalWebServer.ts
@@ -1,24 +1,24 @@
 import { useEffect } from 'react';
 import { CachesDirectoryPath, readFile } from 'react-native-fs';
-// import { BridgeServer } from 'react-native-http-bridge-refurbished';
+import { BridgeServer } from 'react-native-http-bridge-refurbished';
 
 export const useLocalWebServer = (eanbled?: boolean) => {
   useEffect(() => {
     if (!eanbled) return;
-    throw new Error('react-native-http-bridge-refurbished is not supported');
-    // const server = new BridgeServer('http_service', true);
-    // server.get('*', async (req, res) => {
-    //   if (req.url.startsWith('/manifest')) {
-    //     const fileName = req.url.split('/manifest?file=')[1];
 
-    //     const localFile = await readFile(`file://${CachesDirectoryPath}/${fileName}`, 'utf8');
-    //     return res.send(200, 'application/vnd.apple.mpegurl', localFile);
-    //   }
+    const server = new BridgeServer('http_service', true);
+    server.get('*', async (req, res) => {
+      if (req.url.startsWith('/manifest')) {
+        const fileName = req.url.split('/manifest?file=')[1];
 
-    //   return res.send(404, 'text/plain', 'Not Found');
-    // });
-    // server.listen(3000);
+        const localFile = await readFile(`file://${CachesDirectoryPath}/${fileName}`, 'utf8');
+        return res.send(200, 'application/vnd.apple.mpegurl', localFile);
+      }
 
-    // return () => server.stop();
+      return res.send(404, 'text/plain', 'Not Found');
+    });
+    server.listen(3000);
+
+    return () => server.stop();
   }, [eanbled]);
 };
