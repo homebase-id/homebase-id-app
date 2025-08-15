65e2342b219746080b4661ad6a79bd578d30ffdf
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2025-08-15 12:15:09 +0200

    revert passing odinId value

--- packages/mobile/src/pages/login/login-page.tsx
+++ packages/mobile/src/pages/login/login-page.tsx
@@ -266,7 +266,6 @@ const LoginComponent = () => {
           // marginBottom: 16,
         }}
         onChangeText={onChangeOdinId}
-        value={odinId}
         autoCapitalize="none"
         autoCorrect={false}
         onSubmitEditing={onLogin}
